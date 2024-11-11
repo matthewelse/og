337d7cd4b936148186cc399243094822b4c10174
Matthew Else <matthewelse1997@gmail.com>
2024-11-11 09:09:05 +0000

    Simpler node representation

--- lib/nfa.ml
+++ lib/nfa.ml
@@ -7,20 +7,55 @@ module State : sig
   val zero : t
   val succ : t -> t
   val to_int : t -> int
+
+  include Comparable.S_plain with type t := t
 end =
   Int
 
-module Edge = struct
+module Node = struct
   type t =
-    { rule : Character_rule.t option
-    ; target : State.t
+    { rule : (Character_rule.t * State.t) option
+    ; epsilon_rule : State.t option
     }
   [@@deriving sexp_of]
+
+  let empty = { rule = None; epsilon_rule = None }
+
+  let set_rule_exn t rule =
+    assert (Option.is_none t.rule);
+    { t with rule = Some rule }
+  ;;
+
+  let set_epsilon_exn t target =
+    assert (Option.is_none t.epsilon_rule);
+    { t with epsilon_rule = Some target }
+  ;;
+
+  let _fold t ~init:acc ~f =
+    let acc =
+      match t.rule with
+      | None -> acc
+      | Some (rule, to_state) -> f acc (Some rule) to_state
+    in
+    match t.epsilon_rule with
+    | None -> acc
+    | Some to_state -> f acc None to_state
+  ;;
+
+  let exists t ~f =
+    (match t.rule with
+     | None -> false
+     | Some (rule, to_state) -> f (Some rule) to_state)
+    ||
+    match t.epsilon_rule with
+    | None -> false
+    | Some to_state -> f None to_state
+  ;;
 end
 
 module Builder = struct
   type t =
-    { global_ states : Edge.t list Dynarray.t
+    { global_ states : Node.t Dynarray.t
     ; mutable next_state : State.t
     }
   [@@deriving sexp_of]
@@ -34,14 +69,19 @@ module Builder = struct
   ;;
 
   let add_edge t ~from_state ~to_state rule =
-    Dynarray.resize t.states (State.to_int from_state + 1) ~default:(Fn.const []);
-    let edges = Dynarray.get t.states (State.to_int from_state) in
-    Dynarray.set t.states (State.to_int from_state) ({ rule; target = to_state } :: edges)
+    Dynarray.resize t.states (State.to_int from_state + 1) ~default:(Fn.const Node.empty);
+    let node = Dynarray.get t.states (State.to_int from_state) in
+    let node =
+      match rule with
+      | None -> Node.set_epsilon_exn node to_state
+      | Some rule -> Node.set_rule_exn node (rule, to_state)
+    in
+    Dynarray.set t.states (State.to_int from_state) node
   ;;
 end
 
 type t =
-  { nodes : Edge.t iarray iarray
+  { nodes : Node.t iarray
   ; accepting_state : State.t
   }
 [@@deriving sexp_of]
@@ -50,9 +90,7 @@ let build (f : Builder.t @ local -> State.t) =
   let builder = Builder.create () in
   let accepting_state = f builder in
   let%tydi { states; _ } = builder in
-  let states =
-    Iarray.unsafe_of_array (Dynarray.to_array states) |> Iarray.map ~f:Iarray.of_list
-  in
+  let states = Iarray.unsafe_of_array (Dynarray.to_array states) in
   { nodes = states; accepting_state }
 ;;
 
@@ -62,7 +100,7 @@ let eval t input =
     then true
     else (
       let edges = Iarray.get_exn t.nodes (State.to_int current_state) in
-      Iarray.exists edges ~f:(fun { rule; target } ->
+      Node.exists edges ~f:(fun rule target ->
         match rule with
         | None -> eval_inner t input ~offset ~current_state:target
         | Some rule ->
