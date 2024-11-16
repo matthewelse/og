8e869bd1909898799c88ca4361d59c5182847e1e
Matthew Else <matthewelse1997@gmail.com>
2024-11-16 20:21:05 +0000

    tweak

--- lib/iarray.mli
+++ lib/iarray.mli
@@ -7,4 +7,6 @@ type ('a : any) t = 'a iarray [@@deriving sexp_of]
 include module type of Stdlib_stable.IarrayLabels with type 'a t := 'a t
 
 val fold : 'a iarray -> init:'b -> f:('b -> 'a -> 'b) @ local -> 'b
-external unsafe_of_array : ('a : any). 'a array -> 'a t = "%identity" [@@layout_poly]
+
+external unsafe_of_array : ('a : any). 'a array -> 'a t = "%identity"
+[@@layout_poly] [@@noalloc]
