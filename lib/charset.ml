95bb1bef916b1de54e32f5b9a619ff6b3afb50d7
Matthew Else <matthewelse1997@gmail.com>
2024-11-17 09:44:04 +0000

    small tweaks to charset

--- lib/charset.ml
+++ lib/charset.ml
@@ -7,20 +7,10 @@ let array_ix c = Char.to_int c land 3
 let bit_ix c = Char.to_int c lsr 2
 let bit c = Int64_u.shift_left #1L (bit_ix c)
 
-let%expect_test "bits" =
-  List.map Char.all ~f:(fun c ->
-    let bit = bit c |> Int64_u.to_int64 in
-    let ix = array_ix c in
-    bit, ix)
-  |> List.exn_if_dup ~compare:[%compare: Int64.t * int] ~to_sexp:[%sexp_of: Int64.t * int];
-  [%expect {| |}]
-;;
-
 let of_list cs =
   let arr = [| #0L; #0L; #0L; #0L |] in
   List.iter cs ~f:(fun c ->
     let ix = array_ix c in
-    assert (ix <= 3 && ix >= 0);
     Array.unsafe_set arr ix (Int64_u.logor (bit c) (Array.unsafe_get arr ix)));
   Iarray.unsafe_of_array arr
 ;;
@@ -31,3 +21,12 @@ let mem t c =
 ;;
 
 let sexp_of_t t = Char.all |> List.filter ~f:(mem t) |> [%sexp_of: char list]
+
+let%expect_test "bits" =
+  List.map Char.all ~f:(fun c ->
+    let bit = bit c |> Int64_u.to_int64 in
+    let ix = array_ix c in
+    bit, ix)
+  |> List.exn_if_dup ~compare:[%compare: Int64.t * int] ~to_sexp:[%sexp_of: Int64.t * int];
+  [%expect {| |}]
+;;
